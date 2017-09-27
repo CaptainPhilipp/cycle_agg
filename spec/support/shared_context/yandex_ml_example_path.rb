78e0d1d7ec960c5372ec232c7ab772cd28017aa4
Philipp Sorokin <Ph-s@mail.ru>
2017-09-27 18:36:20 +0300

    User sees list of publications (without bootstrap)

--- spec/support/shared_context/yandex_ml_example_path.rb
+++ spec/support/shared_context/yandex_ml_example_path.rb
@@ -1,3 +1,4 @@
 RSpec.shared_context 'yandex_ml_example_path' do
   let(:yandex_ml_example_path) { "#{Rails.root}/spec/upload_fixtures/yandex_ml_example.xml" }
+  let(:title_from_ynadex_ml) { 'First FA-5300' }
 end
