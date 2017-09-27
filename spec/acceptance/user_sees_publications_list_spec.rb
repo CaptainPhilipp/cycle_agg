9335d878cc8238962565538a39f8de03d534380f
Philipp Sorokin <Ph-s@mail.ru>
2017-09-28 01:05:20 +0300

    Fixes Publication creation, list spec

--- spec/acceptance/user_sees_publications_list_spec.rb
+++ spec/acceptance/user_sees_publications_list_spec.rb
@@ -4,8 +4,11 @@ feature 'User sees publications list' do
   include_context 'yandex_ml_example_path'
   let!(:pricelist) { Pricelist.create attachment: File.new(yandex_ml_example_path) }
 
-  it 'When user open root page, he sees all publications' do
+  scenario 'When user open root page, he sees all publications' do
     visit root_path
-    expect(page).to have_content title_from_ynadex_ml
+
+    within '#page' do
+      expect(page).to have_content title_from_ynadex_ml
+    end
   end
 end
