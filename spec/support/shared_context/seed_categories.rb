d1ac89be5ed9477d52048a619a7baed5f618c952
Philipp Sorokin <Ph-s@mail.ru>
2017-10-01 02:03:17 +0300

    Categories menu custom datastruct

--- spec/support/shared_context/seed_categories.rb
+++ spec/support/shared_context/seed_categories.rb
@@ -6,22 +6,26 @@ RSpec.shared_context 'seed categories' do
       Group.new(0, 'MTB',  'МТБ'),
       Group.new(0, 'ROAD', 'Шоссе'),
 
-      Section.new(1,    'Transmission', 'Трансмиссия', %w[MTB ROAD FUN]),
-      Subsection.new(2, 'Shifters',     'Манетки', %w[MTB ROAD FUN Transmission]),
-      Subsection.new(2, 'Cassetes',     'Кассеты', %w[MTB ROAD FUN Transmission]),
+      Section.new(1, 'Frameset', 'Рамы и фреймсеты',   %w[MTB ROAD]),
+      Subsection.new(2, 'Forks', 'Вилки',              %w[MTB ROAD] << 'Frameset'),
+      Subsection.new(2, 'Suspensions', 'Амортизаторы', %w[MTB]      << 'Frameset'),
 
-      Section.new(1,    'Guard',     'Защита',   %w[MTB]),
-      Subsection.new(2, 'Rockrings', 'Рокринги', %w[MTB Guard])
+      Section.new(1, 'Guard', 'Защита',          %w[MTB]),
+      Subsection.new(2, 'Rockrings', 'Рокринги', %w[MTB] << 'Guard')
     ]
   end
 
   let!(:seed) { SeedService.new.call seed_category_structs }
 
-  let(:category_group)         { Category.groups.first }
-  let(:category_sections)      { Category.sections }
-  let(:category_group_section) { Category.sections.first }
-  let(:category_subsection)    { Category.subsections.first }
+  let(:group_mtb)  { SportGroup.all.first }
+  let(:group_road) { SportGroup.all.last }
 
-  let(:category_group_with_hided_section) { Category.groups.last }
-  let(:category_hided_section) { Category.sections.last }
+  let(:group_mtb_sections)  { Category.sections }
+  let(:group_road_sections) { Category.sections.first }
+
+  let(:section_frameset) { Category.sections.first }
+  let(:section_guard)    { Category.sections.first }
+
+  let(:subsections_of_mtb_framesets)  { Category.subsections.first(2) }
+  let(:subsections_of_road_framesets) { Category.subsections.first }
 end
